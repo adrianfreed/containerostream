aa260cf08c43509e5dfd986ea84cf6fc5112ab53
Adrian Freed <adrian@adrianfreed.com>
2020-09-25 18:58:26 -0700

    First release

--- cmake-build-debug-coverage/CMakeFiles/test.dir/DependInfo.cmake
+++ cmake-build-debug-coverage/CMakeFiles/test.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/adrian2013/CLionProjects/test/main.cpp" "/Users/adrian2013/CLionProjects/test/cmake-build-debug-coverage/CMakeFiles/test.dir/main.cpp.o"
+  "/Users/adrian2013/CLionProjects/containerostream/test.cpp" "/Users/adrian2013/CLionProjects/containerostream/cmake-build-debug-coverage/CMakeFiles/test.dir/test.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
